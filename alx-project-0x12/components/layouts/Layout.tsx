ced9f74cd8cf40527393d646bb6ee81d37ecec65
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-12-05 10:01:49 +0100

    Creating Custom hook

---
+++ alx-project-0x12/components/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import { ReactComponentProps } from "@/interfaces";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout: React.FC<ReactComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen bg-gray-600 p-4">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
